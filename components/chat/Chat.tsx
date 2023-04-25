08354e5764e6d868831f92cb9fa33ad62bc02c4d
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-04-25 19:42:21 +0430

    message textarea sticked to bottom

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -25,7 +25,7 @@ const Chat = () => {
     [messages]
   );
   return (
-    <div className=" w-4/5 max-md:w-full py-5 pl-12 max-md:px-5 bg-background dark:bg-background-dark ">
+    <div className=" w-4/5 relative max-md:w-full py-5 pl-12 max-md:px-5 bg-background dark:bg-background-dark ">
       <div dir="rtl" className="chat-box h-[700px] overflow-y-auto py-10 ">
         <div dir="ltr" className="space-y-6 pl-40 max-md:pl-0">
           {messages.map((message: any) => (
--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -9,23 +9,26 @@ interface ChatInputProps {
 
 const ChatInput: React.FC<ChatInputProps> = ({ onSubmit }) => {
   const [input, setInput] = useState<string>();
-  const handleInput = useCallback(
-    (data: string) => {
-      setInput(data);
-    },
-    []
-  );
+  const handleInput = useCallback((data: string) => {
+    setInput(data);
+  }, []);
   return (
-    <div className="flex my-5 justify-between w-full items-center rounded-full bg-white px-4 py-3 dark:bg-primary-dark  ">
-      <HiPaperAirplane
-        onClick={() => {
-          onSubmit({ id: cuid(), isBot: false, message: input });
-          setInput('')
-        }}
-        size={30}
-        className="text-primary scale-x-[-1] cursor-pointer mx-5 dark:text-secondary"
-      />
-      <Input onChange={handleInput} value={input} placeholder="از لیبرا بپرسید" />
+    <div className="py-4 sticky bottom-0 ">
+      <div className="flex my-5 justify-between w-full items-center rounded-full bg-white px-4 py-3 dark:bg-primary-dark  ">
+        <HiPaperAirplane
+          onClick={() => {
+            onSubmit({ id: cuid(), isBot: false, message: input });
+            setInput("");
+          }}
+          size={30}
+          className="text-primary scale-x-[-1] cursor-pointer mx-5 dark:text-secondary"
+        />
+        <Input
+          onChange={handleInput}
+          value={input}
+          placeholder="از لیبرا بپرسید"
+        />
+      </div>
     </div>
   );
 };
