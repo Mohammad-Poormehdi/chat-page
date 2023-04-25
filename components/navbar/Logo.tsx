6ea81f183547cd1298a07a70c5a05ded27f6b079
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-04-25 21:21:47 +0430

    logo centered

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -5,7 +5,7 @@ const orbitron = Orbitron({ subsets: ["latin"] });
 
 const Logo = () => {
   return (
-    <h1 className={"text-3xl text-primary dark:text-white mx-auto max-md:hidden "+orbitron.className}>LIBERO</h1>
+    <h1 className={"text-3xl absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-primary dark:text-white mx-auto max-md:hidden "+orbitron.className}>LIBERO</h1>
   );
 };
 export default Logo;
